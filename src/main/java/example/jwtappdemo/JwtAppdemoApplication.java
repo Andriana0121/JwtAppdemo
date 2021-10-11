403993b809543fc79397843e3650be78b138b599
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-11 11:42:26 +0300

    Initial commit

---
+++ src/main/java/example/jwtappdemo/JwtAppdemoApplication.java
@@ -0,0 +1,13 @@
+package example.jwtappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAppdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAppdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/jwtappdemo/JwtAppdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package example.jwtappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
